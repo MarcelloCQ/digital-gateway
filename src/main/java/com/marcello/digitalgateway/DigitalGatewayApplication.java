d911c2544901e2df43ee486b49127aa6a7f966c3
Marcello Crescimbeni <giosuemc@gmail.com>
2026-04-07 01:47:16 -0500

    first commit

---
+++ src/main/java/com/marcello/digitalgateway/DigitalGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.marcello.digitalgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalGatewayApplication.class, args);
+    }
+
+}
